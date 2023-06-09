f3c59b05801e152b4fa4de58ca102bfa63501c73
Josue Lopez <josuedlopez1982@gmail.com>
2023-06-09 16:02:16 -0400

    Comunicación  entre componentes. PROPS

--- 03_counter_app_vite/src/main.jsx
+++ 03_counter_app_vite/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HelloWorld } from './HelloWorld';
+/* import { HelloWorld } from './HelloWorld'; */
 import { FirstApp } from './FirstApp';
 import './styles.css'
 /* import HelloWorldApp from './HelloWorld' */ /* Puesto que exporté la App por defecto no lleva {} la importación. De no ser así tendría que estar entre {} */
@@ -8,7 +8,10 @@ import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <HelloWorld /> {/* debe ser wl mismo nombre que use en el import. Lo que yo quiera */}
-        <FirstApp /> 
+        {/* <HelloWorld /> debe ser el mismo nombre que use en el import. Lo que yo quiera */}
+        <FirstApp
+            title = 'Ejercício de PROPS'
+            subTitle =  'Pruebas'
+        /> 
     </React.StrictMode>
 )

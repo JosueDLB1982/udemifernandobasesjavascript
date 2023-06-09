33b125e3f22211c1fd6eb32d3ff9cde03552f5fe
Josue Lopez <josuedlopez1982@gmail.com>
2023-06-09 18:17:22 -0400

    add <CounterApp.jsx/>

--- 03_counter_app_vite/src/main.jsx
+++ 03_counter_app_vite/src/main.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 /* import { HelloWorld } from './HelloWorld'; */
-import { FirstApp } from './FirstApp';
+import { CounterApp } from './CounterApp';
 import './styles.css'
+
 /* import HelloWorldApp from './HelloWorld' */ /* Puesto que exporté la App por defecto no lleva {} la importación. De no ser así tendría que estar entre {} */
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        {/* <HelloWorld /> debe ser el mismo nombre que use en el import. Lo que yo quiera */}
-        <FirstApp
-            title = 'Valores por defecto de PROPS'
-            /* subTitle =  'Pruebas' */
-        /> 
+        <CounterApp
+            value = {0}
+        />
     </React.StrictMode>
 )

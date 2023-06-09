9ef77e6faea993292fbc2db4cf9c2bb62275a4e5
Josue Lopez <josuedlopez1982@gmail.com>
2023-06-09 13:33:37 -0400

    inyectando en el HTML el retorno de una finción

--- 03_counter_app_vite/src/FirstApp.jsx
+++ 03_counter_app_vite/src/FirstApp.jsx
@@ -1,8 +1,26 @@
+const newMessage = 'Mensaje inyectado en el HTML'
+const objMessage = {
+    message: 'Hola mundo',
+    title: 'Inyeccion de objeto en el HTML',
+}
+
+const greeting = (name) => {
+    return (
+        `Saludos a todos, mi nombre es ${name}`
+    )
+}
+
 export const FirstApp = () => {
+
     return (
         <> {/* Fragmento de React */}
             <div>Ejercicio de creación de componente</div>
             <p>Prueba de fragmento</p>
+            <p>{newMessage}</p> {/* Se usan {} para inyectar contenido */}
+            <p>{objMessage.message}</p>
+            <p>{objMessage.title}</p>
+            <code>{JSON.stringify(objMessage)}</code>
+            <h1>{greeting('Josue López')}</h1>
         </> 
     )
 } 

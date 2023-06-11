c451c6bd589617f3c1571196ae48911f67127f44
Josue Lopez <josuedlopez1982@gmail.com>
2023-06-11 14:58:41 -0400

    add useState
    se añade useState para el control de lista de categorías

--- 04_gif_expert_app/src/GifExpertApp.jsx
+++ 04_gif_expert_app/src/GifExpertApp.jsx
@@ -1,9 +1,21 @@
-
+import { useState } from "react";
 
 export const GifExpertApp = () => {
+
+const [categories, setCategories] = useState(['Demon Slayer', 'One Punch']);
+
+console.log(categories)
+
   return (
     <>
         <h1>GifExpertApp</h1>
+        <ol>
+            {
+                categories.map((category, i) => {
+                    return <li key={i}>{category}</li>
+                })
+            }
+        </ol>
     </>
   )
 }

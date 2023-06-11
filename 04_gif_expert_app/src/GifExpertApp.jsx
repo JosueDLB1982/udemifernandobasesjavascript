26d2d048e34c220617c9b466c2611463bbd8bdc5
Josue Lopez <josuedlopez1982@gmail.com>
2023-06-11 15:50:51 -0400

    add función de añadir categoria

--- 04_gif_expert_app/src/GifExpertApp.jsx
+++ 04_gif_expert_app/src/GifExpertApp.jsx
@@ -3,12 +3,18 @@ import { useState } from "react";
 export const GifExpertApp = () => {
 
 const [categories, setCategories] = useState(['Demon Slayer', 'One Punch']);
+const onAddCategory = () => {
+    /* console.log('Samuray X') */
+    setCategories(['Samuray X', ...categories]) /* usamos spread operator para crear una copia del array y añadir el elemento nuevo */
+    // setCategories(cat => [...cat, 'Samuray X']) /* Usando un callback. Otra forma de hacerlo */
+}
 
 console.log(categories)
 
   return (
     <>
         <h1>GifExpertApp</h1>
+        <button onClick={ onAddCategory }> AGREGAR </button>
         <ol>
             {
                 categories.map((category, i) => {
